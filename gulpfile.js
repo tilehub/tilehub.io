aae9866fd98fffb9f95fbf17dccc2c1b3e6baf01
Markus Re1 <markus@re1.at>
2018-01-26 12:00:42 +0100

    Add more specific content about the project and team.

--- gulpfile.js
+++ gulpfile.js
@@ -63,25 +63,8 @@ gulp.task('minify-js', function() {
 // Copy vendor files from /node_modules into /vendor
 // NOTE: requires `npm install` before running!
 gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('vendor/bootstrap'))
-
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
-
-  gulp.src(['node_modules/magnific-popup/dist/*'])
-    .pipe(gulp.dest('vendor/magnific-popup'))
-
   gulp.src(['node_modules/scrollreveal/dist/*.js'])
-    .pipe(gulp.dest('vendor/scrollreveal'))
-
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
+      .pipe(gulp.dest('vendor/scrollreveal'));
 
   gulp.src([
       'node_modules/font-awesome/**',
@@ -92,7 +75,7 @@ gulp.task('copy', function() {
       '!node_modules/font-awesome/*.json'
     ])
     .pipe(gulp.dest('vendor/font-awesome'))
-})
+});
 
 // Default task
 gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
@@ -102,9 +85,9 @@ gulp.task('browserSync', function() {
   browserSync.init({
     server: {
       baseDir: ''
-    },
+    }
   })
-})
+});
 
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
